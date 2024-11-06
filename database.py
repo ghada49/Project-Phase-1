2cefb34814fc5fddbaac08cf624533fc5e191993
codaya2005 <ayaelhajj2005@gmail.com>
2024-11-06 18:59:30 +0200

    Update database.py

--- database.py
+++ database.py
@@ -9,7 +9,10 @@ cursor = db.cursor()
 
 cursor.execute("CREATE TABLE if not exists Users(user_id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT, username TEXT UNIQUE, email TEXT UNIQUE, password TEXT,status TEXT CHECK(status IN ('Online', 'Offline')))")
 
-
+'''
+1)	The Users table stores information about sellers and buyers, namely: [username, name, email, password] that are chosen by the user himself along with a [user id] assigned by the code (auto incrementing integer) and [status] which is also set by the codes. The status is online when the client logs in and offline otherwise. 
+Note: the username is unique and two different users can not have the same username. If a user tries to register (sign up) with a username already taken, the program will prompt them to choose another username. Additionally, a mail can be used only once, meaning that after a user registered with a mail, this mail cannot be used again for another account.
+'''
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Products (
@@ -23,7 +26,10 @@ cursor.execute('''
         FOREIGN KEY(user_id) REFERENCES Users(user_id) ON DELETE CASCADE
     )
     ''')
-
+'''
+2)	The Products table stores information about products added into the AUBoutique, namely: [product name, description of the product, price, image of the product] which are set by the sellers along with [product id] and [status] which are assigned by the program. 
+Note: The product id is an auto incrementing integer which is unique for each product added. The unique ID prevents confusion between products with the same name and we do not need to provide too many details when buying one product. Instead, we only provide the ID of the object instead. For the status, it indicates if the object is sold or available. 
+'''
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Transactions (
@@ -35,7 +41,9 @@ cursor.execute('''
         FOREIGN KEY(product_id) REFERENCES Products(product_id)
     )
     ''')
-
+'''
+3)	The Transactions table which records all transactions between users. It includes the transaction id, buyer id (which is the user id of the buyer), product id, transaction date.
+'''
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Messages (
@@ -48,8 +56,10 @@ cursor.execute('''
         FOREIGN KEY(receiver_id) REFERENCES Users(user_id)
                )
                ''')
+'''
+4)	The Messages table which stores messages exchanged between users. It includes the sender id (which is the user id of the sender of a message), receiver id (similarly), the message date, the message id number (set by the code), content.
+
+'''
 
 
 db.commit()
-
-
